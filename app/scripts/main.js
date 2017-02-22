29f4d68233d36dfa1f1beae913f331c915ae8496
KyleNw <kylejwilson@knights.ucf.edu>
2017-02-21 21:52:04 -0500

    New Repo for D3 Test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,25 @@
-console.log('\'Allo \'Allo!');
+
 
 // switching between pages and the light box are here //
 
   $('document').ready(function(){
       $('.scrollspy').scrollSpy();
-       $('.button-collapse').sideNav();
-
+      $('.button-collapse').sideNav();
+      
 
 
 // show and hide pages
        $('#page2').hide();
        $('#page3').hide();
        $('#study').hide();
+       getGraph();
+       
 
        $('.home').click(function(){
-           
+            
+            $('#page1').fadeIn(2500);
             $('#page2').hide();
             $('#page3').hide();
-            $('#page1').fadeIn(2500);
             $('#study').hide()
            
        });
@@ -46,6 +48,7 @@ console.log('\'Allo \'Allo!');
             $('#page3').hide();
             $('#page1').hide();
        })
+      
 
   });
 
@@ -86,7 +89,50 @@ span.onclick = function() {
 
 // d3 famo
 
+var getGraph = function(){
 
+HorizontalBarGraph = function(el, series) {
+  this.el = d3.select(el);
+  this.series = series;
+};
+
+HorizontalBarGraph.prototype.draw = function() {
+  var x = d3.scale.linear()
+    .domain([0, d3.max(this.series, function(d) { return d.value })])
+    .range([0, 100]);
+
+  var segment = this.el
+    .selectAll('.horizontal-bar-graph-segment')
+      .data(this.series)
+    .enter()
+      .append('div').classed('horizontal-bar-graph-segment', true);
+
+  segment
+    .append('div').classed('horizontal-bar-graph-label', true)
+      .text(function(d) { return d.label });
+
+  segment
+    .append('div').classed('horizontal-bar-graph-value', true)
+      .append('div').classed('horizontal-bar-graph-value-bar', true)
+        .style('background-color', function(d) { return d.color })
+        .text(function(d) { return d.inner_label ? d.inner_label : '' })
+        .transition()
+          .duration(1000)
+          .style('min-width', function(d) { return x(d.value) + '%' });
+
+};
+
+var graph = new HorizontalBarGraph('#my-graph', [
+  {label: 'Html', inner_label: 'pro', value: 100, color: '#6ea6df' },
+  {label: 'Css',  inner_label: 'pro 95%',   value: 95,  color: '#84c26d' },
+  {label: 'Blah',  inner_label: 'just okay',   value: 86,  color: '#e17a69' },
+  {label: 'Blah',  inner_label: 'just okay',   value: 93,  color: '#e17a69' },
+  {label: 'Blah',  inner_label: 'just okay',   value: 33,  color: '#e17a69' }
+]);
+graph.draw();
+
+
+};
 
   
 
