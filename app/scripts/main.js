c37ffe5949dae5ed83dc54117d81709e647b787d
KyleNw <kylejwilson@knights.ucf.edu>
2017-02-28 00:43:05 -0500

    New bio layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,10 @@
        $('#page2').hide();
        $('#page3').hide();
        $('#study').hide();
-       getGraph();
+       $('#skillz').hide();
+       $('#workHistory').hide();
+       $('#educationPage').hide();
+       $('#bioPage').hide();
        
 
        $('.home').click(function(){
@@ -21,32 +24,91 @@
             $('#page2').hide();
             $('#page3').hide();
             $('#study').hide()
-           
+            $('#skillz').hide();
+            $('#workHistory').hide();
+            $('#educationPage').hide();
+            $('#bioPage').hide();
+       });
+       
+       $('.goSkillz').click(function(){
+            $('#skillz').fadeIn(1000);
+            $('#page1').hide();
+            $('#page2').hide();
+            $('#page3').hide();
+            $('#study').hide()
+            $('#workHistory').hide();
+            $('#educationPage').hide();
+            $('#bioPage').hide();
+            getGraph();
+       });
+       
+       $('.goHistory').click(function(){
+            $('#workHistory').fadeIn(1000);
+            $('#page1').hide();
+            $('#page2').hide();
+            $('#page3').hide();
+            $('#study').hide()
+            $('#skillz').hide();
+            $('#educationPage').hide();
+            $('#bioPage').hide();
+       });
+
+       $('.goEdo').click(function(){
+            $('#educationPage').fadeIn(1000);
+            $('#page1').hide();
+            $('#page2').hide();
+            $('#page3').hide();
+            $('#study').hide()
+            $('#skillz').hide();
+            $('#workHistory').hide();
+            $('#bioPage').hide();
+            
+       });
+
+       $('.goBio').click(function(){
+            $('#bioPage').fadeIn(1000);
+            $('#page1').hide();
+            $('#page2').hide();
+            $('#page3').hide();
+            $('#study').hide()
+            $('#skillz').hide();
+            $('#workHistory').hide();
+            $('#educationPage').hide();
+            
+            
        });
 
         $('.page2Btn').click(function(){
-           
-           $('#page2').fadeIn(1000);
+            $('#workHistory').hide();
+            $('#page2').fadeIn(1000);
             $('#page3').hide();
             $('#page1').hide();
             $('#study').hide()
-            
+            $('#skillz').hide();
+            $('#educationPage').hide();
+            $('#bioPage').hide();
            
        });
 
         $('.page3Btn').click(function(){
-           
+            $('#workHistory').hide();
             $('#page2').hide();
             $('#page3').fadeIn(1000);
             $('#page1').hide();
             $('#study').hide()
-           
+            $('#skillz').hide();
+            $('#educationPage').hide();
+           $('#bioPage').hide();
        });
        $('#goStudy').click(function(){
             $('#study').fadeIn(1000);
             $('#page2').hide();
             $('#page3').hide();
             $('#page1').hide();
+            $('#skillz').hide();
+            $('#workHistory').hide();
+            $('#educationPage').hide();
+            $('#bioPage').hide();
        })
       
 
